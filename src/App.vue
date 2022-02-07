dd6597c874cae978c863fc3d7fdf9c140747fa15
conectis360 <conectis360@gmail.com>
2022-02-06 21:42:39 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
+<!-- <template> contains all the markup structure and display logic of your component. -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do list</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+  <ul>
+    <li v-for="item in ToDoItems" :key="item.id">
+      <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+    </li>
+  </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+import ToDoForm from './components/ToDoForm';
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ToDoItem,
+    ToDoForm,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: "Learn Vue", done: false },
+        { id: uniqueId('todo-'), label: "Create a Vue project with the CLI", done: true },
+        { id: uniqueId('todo-'), label: "Have fun", done: true },
+        { id: uniqueId('todo-'), label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+    }      
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// its purpose is to register global components and also additional vue libraries.
 createApp(App).mount('#app')
