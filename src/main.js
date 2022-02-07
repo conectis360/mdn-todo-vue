9f91fbc0e0129a3dc3f8c289b2ef6046e93bd4e1
conectis360 <conectis360@gmail.com>
2022-02-06 21:50:47 -0300

    using a file to add style

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/reset.css';
+
+
 // its purpose is to register global components and also additional vue libraries.
 createApp(App).mount('#app')
